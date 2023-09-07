a0e397492fa6db78e01454341be8c249c43c70c3
vsnyc <vinod.k.shukla@gmail.com>
2023-09-06 22:54:33 -0400

    Updated cfn-lint command

--- .project_automation/static_tests/entrypoint.sh
+++ .project_automation/static_tests/entrypoint.sh
@@ -6,4 +6,11 @@ PROJECT_PATH=${BASE_PATH}/project
 PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
 
 cd $PROJECT_PATH
-cfn-lint -t templates/**/*.yaml -a /tmp/qs-cfn-lint-rules/qs_cfn_lint_rules/
+
+# Ignoring the following for migration
+# All warnings,
+# E1019 - Sub validation - false positive for conditionals,
+# E2521 - required properties, E3002 - resource properties - false positive for newer resources than pinned CloudFormation resource spec
+# E3005 - DependsOn - false positive for conditionals,
+# E9101 - Inclusive language check - false positive for database resources
+cfn-lint --ignore-checks W,E1019,E2521,E3002,E3005,E9101 -t templates/**/*.yaml -a ~/qs-cfn-lint-rules/qs_cfn_lint_rules/
